91617d2a8bc22e5576949c5a1ec0d375deaaa1f1
PRATYUSH <53640877+pratz222@users.noreply.github.com>
2025-03-06 10:39:51 +0530

    Update footer.jsx

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            Developer Portfolio by <span className="text-[#16f2b3]">PRATYUSH</span>
+            Portfolio by <span className="text-[#16f2b3]">PRATYUSH</span>
           </p>
           <div className="flex items-center gap-5">
             <Link
