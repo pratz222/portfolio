bf846f20ea2b9b2465c87026a91c622936e68f2a
PRATYUSH <53640877+pratz222@users.noreply.github.com>
2025-11-02 23:15:26 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,27 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Embedded Software Engineer [Avionics] Intern',
+    title: 'Embedded Systems Engineer [Avionics]',
+    company: "Ideaforge Technology Ltd.",
+    duration: "July 2025 - Present"
+  },
+  {
+    id: 2,
+    title: 'Embedded System Intern [Avionics]',
     company: "Ideaforge Technology Ltd.",
     duration: "Jan 2025 - July 2025"
   },
   {
-    id: 2,
+    id: 3,
     title: "Embedded Software Developer Intern",
     company: "HeARThealth Technologies",
     duration: "Oct 2023 - Dec 2023"
   },
   {
-    id: 3,
+    id: 4,
     title: "Embedded System & IoT Intern",
     company: "AiROBOSOFT Products And Services Ltd.",
     duration: "Oct 2023 - Jan 2024"
   }
 ]
+
